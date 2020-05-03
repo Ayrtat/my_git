aae7c18cc515e9ef753e7b3bc31360ed58789125
Ayrtat <36817521+Ayrtat@users.noreply.github.com>
2020-05-03 17:19:05 +0300

    task from gfg
    https://www.geeksforgeeks.org/ugly-numbers/

---
+++ C_plus_plus/ugly_numbers.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <map>
+
+bool checkOnPrimaries(int num)
+{
+    std::vector<int> divisors = {2, 3, 5};
+    
+    while(num)
+    {
+        auto exists = false;
+        auto divideOn = 1;
+        
+        for(auto divisor : divisors)
+        {
+            if(num % divisor == 0) 
+            {
+                exists = true;
+                divideOn = divisor;
+                break;
+            }
+        }
+        
+        if(exists) 
+            num /= divideOn;
+        else 
+            break;
+    }
+    
+    return num == 1 ? true : false;
+}
+
+int uglyNumber(int n)
+{
+    switch(n)
+    {
+        case 2:
+            return 2;
+        case 3:
+            return 3;
+        case 5:
+            return 5;
+    }
+    
+    
+    int previousUgly = uglyNumber(n - 1);
+    auto nextConvinient = previousUgly + 1;
+    
+    while(!checkOnPrimaries(nextConvinient))
+    {
+        nextConvinient++;
+    }
+    
+    return nextConvinient;
+}
+
+int main()
+{
+    int number;
+    std::cin >> number;
+    
+    std::cout << uglyNumber(number);
+}
