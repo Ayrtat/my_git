7f8180d90d4d81da0832849c84a556508a32ad9e
Ayrtat <36817521+Ayrtat@users.noreply.github.com>
2020-11-21 14:02:46 +0300

    Update nextPermutationWithRepetition.cpp

--- C_plus_plus/nextPermutationWithRepetition.cpp
+++ C_plus_plus/nextPermutationWithRepetition.cpp
@@ -8,28 +8,21 @@
 #include <list>
 #include <map>
 
-template<typename InputIt>
-bool nextPermutationWithRepetition(InputIt begin,
-                                    InputIt end,
-                                    typename InputIt::value_type from_value,
-                                    typename InputIt::value_type to_value) {
-    auto stop_iteration = std::all_of(begin, end, [&to_value](auto value) { return (value == to_value); });
+template<typename InputIt, typename T>
+bool nextPermutationWithRepetition(InputIt begin, InputIt end, T from_value, T to_value) {
+	auto it = std::find_if_not(std::make_reverse_iterator(end),
+							   std::make_reverse_iterator(begin),
+							   [&to_value](auto current) { return to_value == current; });
 
-    if(stop_iteration)
-        return  false;
+	if (it == std::make_reverse_iterator(begin))
+		return false;
 
-    typename InputIt::value_type search_this_value[] = { to_value };
+	auto bound_element_iterator = std::prev(it.base());
 
-    auto bound_element_iterator = std::find_end(begin,
-                                                end,
-                                                std::begin(search_this_value),
-                                                std::end(search_this_value),
-                                                [](auto left, auto right) { return left != right; });
+	(*bound_element_iterator)++;
+	std::fill(std::next(bound_element_iterator), end, from_value);
 
-    (*bound_element_iterator)++;
-    std::fill(std::next(bound_element_iterator), end, from_value);
-
-    return true;
+	return true;
 }
 
 int main() {
