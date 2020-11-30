e58adeb477e067ccc7fce3a864bc4c4b493c4bad
Ayrtat <36817521+Ayrtat@users.noreply.github.com>
2020-11-30 16:26:51 +0300

    Update nextPermutationWithRepetition.cpp

--- C_plus_plus/nextPermutationWithRepetition.cpp
+++ C_plus_plus/nextPermutationWithRepetition.cpp
@@ -25,6 +25,31 @@ bool nextPermutationWithRepetition(InputIt begin, InputIt end, T from_value, T t
 	return true;
 }
 
+template<typename InputIt, typename ContainerT>
+bool nextPermutationWithRepetition_v(InputIt begin, InputIt end, ContainerT from_value, ContainerT to_value) {
+    if(std::distance(begin, end) != std::size(from_value))
+        return false;
+    if(std::distance(begin, end) != std::size(to_value))
+        return false;
+
+    const auto pairOfIt = std::mismatch(std::make_reverse_iterator(end),
+                                        std::make_reverse_iterator(begin),
+                                        std::make_reverse_iterator(std::end(to_value)));
+
+    auto it = pairOfIt.first;
+
+    if (it == std::make_reverse_iterator(begin))
+        return false;
+
+    auto bound_element_iterator = std::prev(it.base());
+    auto dist = std::distance(begin, bound_element_iterator);
+
+    (*bound_element_iterator)++;
+    std::copy(std::begin(from_value) + dist, std::end(from_value), std::next(bound_element_iterator));
+
+    return true;
+}
+
 int main() {
     std::list<int> vec(3, 0);
 
